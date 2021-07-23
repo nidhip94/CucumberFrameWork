a7d7a487b9149d48d84fba8a1f1fa6149ecf79e5
Nidhi <nidhi.prajapati@navihealth.com>
2021-07-23 23:30:03 +0530

    created features file

---
+++ src/test/java/stepDefinition/stepDefination.java
@@ -0,0 +1,27 @@
+package stepDefinition;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class stepDefination {
+	
+	
+	@Given("User is in NetBanking Login Page")
+	public void User_is_in_Login_Page(){
+		
+	}
+	
+	@When("User logins to the application with correct username and password")
+	public void login(){
+		
+	}
+	
+	@Then("Home page is populated")
+	public void homePageShown(){
+		
+	}
+	
+	
+
+}
